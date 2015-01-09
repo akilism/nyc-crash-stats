05fd059e804f5bad9bbd1ecd7051f682ce50021b
akil harris <akilism@chillchillchillchillchillchill.com>
2015-01-08 21:43:12 -0500

    year fix

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,175 +1,25 @@
 'use strict';
 
 angular.module('nycCrashStatsApp')
-  .controller('MainCtrl', ['$scope', 'crashStats', 'Socrata', function($scope, crashStats, Socrata) {
-
-    var addValues = function(currVals, newVals, keys) {
-      var totaledVals = currVals;
-      _.forEach(keys, function(key) {
-        totaledVals[key] += newVals[key];
-      });
-
-      return totaledVals;
-    };
-
-    var compressTotalsByYear = function(totals)  {
-      var years = {};
-      var topKeys = ['number_of_cyclist_injured', 'number_of_cyclist_killed', 'number_of_motorist_injured', 'number_of_motorist_killed', 'number_of_pedestrians_injured', 'number_of_pedestrians_killed', 'number_of_persons_injured', 'number_of_persons_killed', 'result_death', 'result_injury', 'total_accidents'];
-      _.forEach(totals, function (total) {
-        if(years[total.year]) {
-          years[total.year].factors = addValues(total.factors, years[total.year].factors, _.keys(total.factors));
-          years[total.year].times = addValues(total.times, years[total.year].times, _.keys(total.times));
-          years[total.year].vechicles = addValues(total.vehicles, years[total.year].vehicles, _.keys(total.vehicles));
-          _.forEach(topKeys, function(key) {
-            years[total.year][key] += total[key];
-          });
-        } else {
-          years[total.year] = _.cloneDeep(total);
-        }
-      });
-      return years;
-    };
-
-    var getPctTotal = function(value, total) {
-      value = value || 0;
-      // console.log(value, total, value/total * 100);
-      return (value/total * 100).toPrecision(2);
-    };
-
-    var getItemTotals = function (totals) {
-      var re = /\//g;
-
-      var accTotal = _.reduce(totals, function(result, total) {
-        total = total || 0;
-        result += total;
-        return result;
-      }, 0);
-
-      return _.map(totals, function(v, k) {
-        return {
-          count: v || 0,
-          label: k.replace(re, ' / ').toLowerCase(),
-          pctTotal: getPctTotal(v, accTotal),
-          pctChange: 0
-        };
-      });
-    };
-
-    var buildItems = function(totals, type) {
-      var formattedItems = [];
-      formattedItems[0] = {};
-      formattedItems[1] = {};
-
-      switch(type) {
-        case 'crash':
-          formattedItems[0].count = totals.result_injury;
-          formattedItems[0].label = 'Injury';
-          formattedItems[0].stickyLabel = 'sticky-label';
-          formattedItems[0].icon = '';
-          formattedItems[0].showIcon = false;
-          formattedItems[1].count = totals.result_death;
-          formattedItems[1].label = 'Death';
-          formattedItems[1].stickyLabel = 'sticky-label';
-          formattedItems[1].icon = '';
-          formattedItems[1].showIcon = false;
-          break;
-        case 'injury':
-          formattedItems[2] = {};
-          formattedItems[0].count = totals.number_of_pedestrians_injured;
-          formattedItems[0].label = 'Pedestrians';
-          formattedItems[0].stickyLabel = false;
-          formattedItems[0].icon = 'fa fa-user fa-2x';
-          formattedItems[0].showIcon = true;
-          formattedItems[1].count = totals.number_of_cyclist_injured;
-          formattedItems[1].label = 'Cyclists';
-          formattedItems[1].stickyLabel = false;
-          formattedItems[1].icon = 'fa fa-bicycle fa-2x';
-          formattedItems[1].showIcon = true;
-          formattedItems[2].count = totals.number_of_motorist_injured;
-          formattedItems[2].label = 'Motorists';
-          formattedItems[2].stickyLabel = false;
-          formattedItems[2].icon = 'fa fa-car fa-2x';
-          formattedItems[2].showIcon = true;
-          break;
-        case 'death':
-          formattedItems[2] = {};
-          formattedItems[0].count = totals.number_of_pedestrians_killed;
-          formattedItems[0].label = 'Pedestrians';
-          formattedItems[0].stickyLabel = false;
-          formattedItems[0].icon = 'fa fa-user fa-2x';
-          formattedItems[0].showIcon = true;
-          formattedItems[1].count = totals.number_of_cyclist_killed;
-          formattedItems[1].label = 'Cyclists';
-          formattedItems[1].stickyLabel = false;
-          formattedItems[1].icon = 'fa fa-bicycle fa-2x';
-          formattedItems[1].showIcon = true;
-          formattedItems[2].count = totals.number_of_motorist_killed;
-          formattedItems[2].label = 'Motorists';
-          formattedItems[2].stickyLabel = false;
-          formattedItems[2].icon = 'fa fa-car fa-2x';
-          formattedItems[2].showIcon = true;
-          break;
-        case 'factors':
-          formattedItems = getItemTotals(totals);
-          break;
-        case 'vehicles':
-          formattedItems = getItemTotals(totals);
-          break;
-      }
-
-      return formattedItems;
-    };
-
-    var getTotals = function(totals, type) {
-      var formattedTotals = {};
-
-      switch(type) {
-        case 'crash':
-          formattedTotals.total = totals.total_accidents;
-          formattedTotals.title = 'Total Crashes';
-          formattedTotals.items = buildItems(totals, 'crash');
-          break;
-        case 'injury':
-          formattedTotals.total = totals.number_of_persons_injured;
-          formattedTotals.title = 'Total Injuries';
-          formattedTotals.items = buildItems(totals, 'injury');
-          break;
-        case 'death':
-          formattedTotals.total = totals.number_of_persons_killed;
-          formattedTotals.title = 'Total Deaths';
-          formattedTotals.items = buildItems(totals, 'death');
-          break;
-        case 'factors':
-          formattedTotals.title = 'Contributing Factors';
-          formattedTotals.items = buildItems(totals, 'factors');
-          break;
-        case 'vehicles':
-          formattedTotals.total = totals.number_of_persons_killed;
-          formattedTotals.title = 'Vehicle Types';
-          formattedTotals.items = buildItems(totals, 'vehicles');
-          break;
-      }
-
-      return formattedTotals;
-    };
+  .controller('MainCtrl', ['$scope', 'crashStats', 'Socrata', function ($scope, crashStats, Socrata) {
+    crashStats.lastAccidents.title = 'Last ' + crashStats.lastAccidents.length + ' Crashes';
+    crashStats.lastAccidents.id = 'accident';
+    crashStats.lastInjuries.title = (crashStats.lastInjuries.length >= 100) ? 'Last ' + crashStats.lastInjuries.length + ' Crashes Resulting In An Injury' : 'All Crashes Resulting In An Injury';
+    crashStats.lastInjuries.id = 'injury';
+    crashStats.lastDeaths.title = (crashStats.lastDeaths.length >= 100) ? 'Last ' + crashStats.lastDeaths.length + ' Crashes Resulting In A Death' : 'All Crashes Resulting In A Death';
+    crashStats.lastDeaths.id = 'death';
 
     $scope.crashStats = crashStats;
-    var yearlyTotals = compressTotalsByYear(crashStats.totals);
-
-    var d = new Date();
-    $scope.crashStats.crashTotals = getTotals(yearlyTotals[d.getFullYear()], 'crash');
-    $scope.crashStats.injuryTotals = getTotals(yearlyTotals[d.getFullYear()], 'injury');
-    $scope.crashStats.deathTotals = getTotals(yearlyTotals[d.getFullYear()], 'death');
-    $scope.crashStats.factorTotals = getTotals(yearlyTotals[d.getFullYear()].factors, 'factors');
-    $scope.crashStats.vehicleTotals = getTotals(yearlyTotals[d.getFullYear()].vehicles, 'vehicles');
+    // console.log(crashStats.yearly);
+    $scope.year = new Date().getFullYear();
+    $scope.yearly = crashStats.yearly[0];
     $scope.factorListSize = 5;
     $scope.hideInstruction = true;
 
-
-    $scope.setActiveAccident = function(accident, useApply) {
+    $scope.setActiveAccident = function (accident, useApply) {
       // console.log(accident);
       if(useApply){
-        $scope.$apply(function() {
+        $scope.$apply(function () {
           $scope.popup = accident;
           $scope.activeAccident = accident;
         });
@@ -180,7 +30,7 @@ angular.module('nycCrashStatsApp')
       showDetailCrashPopup(event);
     };
 
-    $scope.showAccidentDetails = function(accidentId, withApply) {
+    $scope.showAccidentDetails = function (accidentId, withApply) {
       // console.log(accidentId);
       var accident = getAccident(accidentId);
       console.log('accident', accident);
@@ -190,7 +40,7 @@ angular.module('nycCrashStatsApp')
       }
 
       if (withApply) {
-        $scope.$apply(function() {
+        $scope.$apply(function () {
           $scope.popup = accident;
         });
       } else {
@@ -198,11 +48,11 @@ angular.module('nycCrashStatsApp')
       }
     };
 
-    $scope.getFactorClass = function(factor) {
+    $scope.getFactorClass = function (factor) {
       return 'factor-' + factorMap(factor);
     };
 
-    $scope.setNewClass = function() {
+    $scope.setNewClass = function () {
       if($scope.newAccidents || $scope.newKills || $scope.newInjuries) {
         return 'col-md-6';
       } else {
@@ -211,19 +61,19 @@ angular.module('nycCrashStatsApp')
     };
 
     // Calculate the yearly totals for the filtered accidents.
-    $scope.calculateYearlyStats = function(dataset) {
+    $scope.calculateYearlyStats = function (dataset) {
 
       var yearlyKeys = _.keys($scope.yearly),
       newYearly = {},
       factorsV1 = [],
       factorsV2 = [];
 
-      _.forEach(yearlyKeys, function(key) {
+      _.forEach(yearlyKeys, function (key) {
         if (!newYearly.hasOwnProperty(key)) {
           newYearly[key] = 0;
         }
 
-        newYearly[key] = _.reduce(dataset, function(sum, accident) {
+        newYearly[key] = _.reduce(dataset, function (sum, accident) {
           return sum += parseInt(accident[key], 10);
         }, 0);
       });
@@ -233,20 +83,20 @@ angular.module('nycCrashStatsApp')
       $scope.factorsVehicle1 = calculateFactorTotals(dataset);
     };
 
-    // $scope.showJumbo = function() {
+    // $scope.showJumbo = function () {
     //   $('.jumbotron-wrapper').fadeIn();
     //   ga('send', 'event', 'click', 'about');
     // };
 
-    // $scope.hideJumbo = function() {
+    // $scope.hideJumbo = function () {
     //   $('.jumbotron-wrapper').fadeOut();
     // };
 
     // Handle the click event for the overlay display
     // Community Boards, Neighborhoods, Zip codes etc.
     // Makes the service call to get the correct shape file
-    // Calls the display layer functionto show the shapes
-    $scope.showOverlay = function(type, $event) {
+    // Calls the display layer function to show the shapes
+    $scope.showOverlay = function (type, $event) {
       $('.nav.map-layers li').removeClass('active');
       $($event.target.parentElement).addClass('active');
 
@@ -263,7 +113,7 @@ angular.module('nycCrashStatsApp')
       $scope.$broadcast('displayMapOverlay', type);
     };
 
-    var getActiveType = function(type) {
+    var getActiveType = function (type) {
       switch (type) {
         case 'citycouncil':
           return 'City Council District';
@@ -280,7 +130,7 @@ angular.module('nycCrashStatsApp')
     };
 
     // Maps the contributing factor text to a css class.
-    var factorMap = function(factor) {
+    var factorMap = function (factor) {
       switch(factor.toLowerCase()) {
         case 'uspecified':
           return 'unspecf';
@@ -386,7 +236,7 @@ angular.module('nycCrashStatsApp')
     };
 
     // get all the factors attached to this accident.
-    var getAccidentFactors = function(accident) {
+    var getAccidentFactors = function (accident) {
       var factors = [];
 
       for(var i = 1; i <= 5; i++) {
@@ -401,10 +251,10 @@ angular.module('nycCrashStatsApp')
     };
 
     // Get the additional accidents at this location in the current dataset.
-    var getAdditionalAccidents = function(accidentLocation) {
+    var getAdditionalAccidents = function (accidentLocation) {
       var key = 'lastAccidents';
 
-      return _.filter($scope.crashStats[key], function(accident) {
+      return _.filter($scope.crashStats[key], function (accident) {
         if(accident.hasOwnProperty('location')) {
           if((accident.location.latitude === accidentLocation.latitude) && (accident.location.longitude === accidentLocation.longitude)) {
             return true;
@@ -415,20 +265,20 @@ angular.module('nycCrashStatsApp')
     };
 
     //Fetch an accident by id.
-    var getAccident = function(accidentId) {
+    var getAccident = function (accidentId) {
         var key = 'lastAccidents';
         accidentId = Number(accidentId);
-        return _.find($scope.crashStats[key], function(accident) {
+        return _.find($scope.crashStats[key], function (accident) {
           return (accidentId === accident.unique_key);
         });
     };
 
-    var closeDetailPopup = function() {
+    var closeDetailPopup = function () {
       $('#accidentPopup').css('display', 'none');
       $('#accidentPopup .btn-popup-close').off('click', closeDetailPopup);
     };
 
-    var showDetailCrashPopup = function(event) {
+    var showDetailCrashPopup = function (event) {
       var $$popup = $('#accidentPopup');
       var top = 10;
 
@@ -441,7 +291,7 @@ angular.module('nycCrashStatsApp')
     };
 
     //Calculate the aggregate factor totals for the filtered accidents. Vehicle 1 only right now.
-    var calculateFactorTotals = function(dataset) {
+    var calculateFactorTotals = function (dataset) {
       var factors = {},
         factor = {
           'factor': '',
@@ -456,7 +306,7 @@ angular.module('nycCrashStatsApp')
           'total_killed': 0,
         };
 
-      _.forEach(dataset, function(accident) {
+      _.forEach(dataset, function (accident) {
         if (accident.hasOwnProperty('contributing_factor_vehicle_1')) {
           var factorName = accident.contributing_factor_vehicle_1.trim();
 
@@ -488,11 +338,11 @@ angular.module('nycCrashStatsApp')
         }
       });
 
-      factors = _.sortBy(_.toArray(factors), function(factor) {
+      factors = _.sortBy(_.toArray(factors), function (factor) {
         return factor.total_accidents;
       });
 
-      var reverseArray = function(initialArray) {
+      var reverseArray = function (initialArray) {
         var len = initialArray.length - 1;
         var i = 0;
         var reversedArray = [];
